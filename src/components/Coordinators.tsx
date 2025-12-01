746d78e070e3fa3bea562e50aad84e25ea5c0628
katkurisanjay <sanjaykatkuri2004@gmail.com>
2025-12-01 22:19:41 +0530

    Update Coordinators.tsx

--- src/components/Coordinators.tsx
+++ src/components/Coordinators.tsx
@@ -24,7 +24,7 @@ const Coordinators = () => {
       { 
         name: 'Mohammad Sohail', 
         role: 'Assistant Professor, VCE',
-        img: 'https://res.cloudinary.com/dika0ttaj/image/upload/v1764607430/sirajj_j7nf9j.jpg'
+        img: 'https://res.cloudinary.com/dika0ttaj/image/upload/v1764607739/sohail_jc0qr1.jpg'
       },
       { 
         name: 'Mrs. Zareena Begum', 
